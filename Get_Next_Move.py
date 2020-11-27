b944f5b5ec99e2ebf9a5bc25a53986392c3c6d8c
jakoth <63280686+jakoth@users.noreply.github.com>
2020-11-27 19:05:19 +0530

    Delete Get_Next_Move.py

--- Get_Next_Move.py
+++
@@ -1,7 +0,0 @@
-import json
-from CPU_Files import cpu_level_extreme
-from SetAndAddMovesInRDB import *
-
-
-def get_and_set_next_move(game_ID, from_move, to_moves, result, cpu_side):
-	return cpu_level_extreme()
